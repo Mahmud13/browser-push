893f25663d7e7075f58233df8fff0de2cfd4f98d
Mahmud13 <mahmud.bracu@gmail.com>
2017-04-14 18:41:11 +0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-/ Give the service worker access to Firebase Messaging.
+// Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
 importScripts('https://www.gstatic.com/firebasejs/3.7.6/firebase-app.js');
